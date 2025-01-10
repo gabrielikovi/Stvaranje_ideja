a103dc22bd6e57072669edfa957bf211bcca88c1
gabrielikovi <gabiparadz@gmail.com>
2025-01-10 17:22:57 +0100

    zadatak1

--- idea_sharing_platform/ideas/models.py
+++ idea_sharing_platform/ideas/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
+    is_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
